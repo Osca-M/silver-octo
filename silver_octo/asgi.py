86c39f869d3dc4c10d89fb90a8e274cf4b61b5c9
Osca Mwongera <oscamwongera@gmail.com>
2021-09-24 13:51:24 +0300

    add a websocket url router

--- silver_octo/asgi.py
+++ silver_octo/asgi.py
@@ -9,12 +9,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silver_octo.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+    # Just HTTP and websockets for now. (We can add other protocols later.)
 })
