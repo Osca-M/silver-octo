43eeb7d9d8cfe47f3f81b643c8f70198daade98b
Osca Mwongera <oscamwongera@gmail.com>
2021-09-24 11:57:05 +0300

    Add django_private_chat2 into project

--- silver_octo/asgi.py
+++ silver_octo/asgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django_private_chat2 import urls
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silver_octo.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(urls.websocket_urlpatterns)
+    ),
+})
