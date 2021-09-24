ef741ad6f9c0b87b8aa2b8bb3bea8ed359b1cd14
Osca Mwongera <oscamwongera@gmail.com>
2021-09-24 15:05:23 +0300

    make consumer methods async

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,34 +1,33 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 # noinspection PyAttributeOutsideInit
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
         self.room_name = self.scope.get('url_route', {}).get('kwargs', {}).get('room_name', '')
         self.room_group_name = f'chat_{self.room_name}'
         # Join room Group
-        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
-        self.accept()
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room Group
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from websocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
         message = text_data_json.get('message', '')
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {'type': 'chat_message', 'message': message})
+        await self.channel_layer.group_send(self.room_group_name, {'type': 'chat_message', 'message': message})
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event.get('message', '')
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        await self.send(text_data=json.dumps({'message': message}))
